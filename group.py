ff9fabe17edd8d9038687e70c3ea4d258a81bd7b
pkruzikova <p.kruziku@gmail.com>
2022-10-20 10:51:02 +0100

    Added friend group info

--- group.py
+++ group.py
@@ -1,5 +1,32 @@
 """An example of how to represent a group of acquaintances in Python."""
 
-# Your code to go here...
-
-my_group =
+my_group = {
+    'Jill': {
+        'age': 26,
+        'job': ['biologist'],
+        'connections': {
+            'friend': 'Zalika',
+            'partner': 'John'
+        }},
+    'Zalika': {
+        'age': 28,
+        'job': ['artist'],
+        'connections': {
+            'friend': 'Jill',
+            'landlord': 'Nash'
+        }},
+    'John': {
+        'age': 27,
+        'job': ['writer'],
+        'connections': {
+            'partner': 'Jill',
+            'cousin': 'Nash'
+        }},
+    'Nash': {
+        'age': 34,
+        'job': ['chef'],
+        'connections': {
+            'cousin': 'John',
+            'tenant': 'Zalika'
+        }}
+}
