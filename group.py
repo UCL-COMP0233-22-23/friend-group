e90d36b08a35d5f8dbc3f0bf04e2087a494f6034
Jeremy <jeremy.morgan.22@ucl.ac.uk>
2022-10-20 10:55:43 +0100

    added people to the group

--- group.py
+++ group.py
@@ -2,4 +2,10 @@
 
 # Your code to go here...
 
-my_group = {"John Smith": {"Age": int,"Job": [], "Relationships":{}}}
+my_group = {"Jill": {"Age": 26,"Job": ["biologist"], "Relationships":{"Zalika": "friend", "John": "partner"}}}
+
+my_group["Zalika"] = {"Age": 28, "Job":["artist"],"Relationships":{"Jill":"friend"}}
+my_group["John"] = {"Age": 27, "Job":["writer"],"Relationships":{"Jill":"partner"}}
+my_group["Nash"] = {"Age": 34, "Job":["chef"],"Relationships":{"John":"cousin", "Zalika": "landlord"}}
+
+
