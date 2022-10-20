4a006afc2176dd33483c3167ec921c97a0031543
Alejandro Avila Carrion <thealexabi@gmail.com>
2022-10-20 11:15:24 +0100

    Updated Group file

--- group.py
+++ group.py
@@ -2,4 +2,37 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {
+    'Jill' : {
+        'relationship': {
+            'Zalika': 'friend',
+            'John': 'partner'
+        },
+        'age': 26,
+        'job': 'Biologist'
+    },
+    'Zalika' : {
+        'relationship': {
+            'Jill': 'friend'
+        },
+        'age': 28,
+        'job': 'Artist'
+    },
+    'John' : {
+        'relationship': {
+            'Jill': 'partner'
+        },
+        'age': 27,
+        'job': 'Writer'
+    },
+    'Nash' : {
+        'relationship': {
+            'John': 'cousin',
+            'Zalika': 'landlord'
+        },
+        'age': 34,
+        'job': 'Chef'
+    }
+}
+
+print(",".join([my_group[x]['job'] for x in my_group]))
