2f79ecf3231b1445dc627327c5408bf476d88bfd
Tomas Dienstl <tomasdienstl2001@gmail.com>
2022-10-20 11:06:23 +0100

    first add

--- group.py
+++ group.py
@@ -2,4 +2,16 @@
 
 # Your code to go here...
 
-my_group =
+
+
+jill = {'name': 'Jill', 'age':'26', 'job':'biologist','friend':'Zalika','partner':'john'}
+
+zalika={'name': 'Zalika', 'age':'28', 'job':'artist','friend':'jill'}
+
+john = {'name': 'john', 'age':'27', 'job':'writer','partner':'jill'}
+
+nash={'name': 'Nash', 'age':'34', 'job':'chef','cousin':'john','landlord':'Zalika'}
+
+my_group = {'Jill':jill, 'zalika':zalika, 'john':john, 'nash':nash}
+
+
