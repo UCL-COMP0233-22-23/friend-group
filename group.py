44f512eed432133c9d2cc95c85e5fd00b11c403a
James Kostas Ray <JamesRay2142@gmail.com>
2022-10-20 11:08:33 +0100

    Co-authored-by: nathanhigginbotham <nathanhigginbotham@users.noreply.github.com> Co-authored-by: Alicja Polanska <alicjaap@users.noreply.github.com>

--- group.py
+++ group.py
@@ -2,4 +2,38 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {
+    
+    "John": {
+        "age": 47,
+        "occupation": 'Hunk',
+        "connection": 'He has been alone for the last 30 years'
+    },
+    
+    "Tony": {
+        "age": 60,
+        "occupation": 'Don',
+        "connection": 'He knows everybody' 
+    },
+
+    "Fredo": {
+        "age": 32,
+        "occupation": 'Consigliere',
+        "connection": 'Third in line' 
+    },
+
+    "Michael": {
+        "age": 51,
+        "occupation": 'Underboss',
+        "connection": 'He is the heir' 
+    },
+
+    "Christopher": {
+        "age": 27,
+        "occupation": 'Consigliere',
+        "connection": 'He\'\s Tonys Nephew' 
+    },
+}
+
+print(my_group["John"]["age"])
+
