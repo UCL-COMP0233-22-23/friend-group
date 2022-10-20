dfc67cd7bb83f18949dedd73b5bd770070015afb
Chongfeng Ling <ling.chongfeng@outlook.com>
2022-10-21 00:07:56 +0100

    comprehension expressions with dict and list

--- group.py
+++ group.py
@@ -3,41 +3,50 @@
 # Your code to go here...
 
 my_group = {
-    "Jill" : {
+    "Jill": {
         "age": 26,
         "job": "biologist",
-        "connection": {
-            "friend": "Zalika",
-            "partner": "John"
-        }
-    },
-    "Zalika" : {
-        "name": "Zalika",
-        "age": "28",
-        "job": "artist",
-        "connection": {
-            "friend": "Jill",
-            "partner": ""
-        }
-    },
-    "John" : {
-        "name": "John",
-        "age": "27",
-        "job": "writer",
-        "connection": {
-            "friend": "",
-            "partner": "Jill"
-        }
-    },
-    "Nash" : {
-        "name": "Nash",
-        "age": "34",
-        "job": "chef",
-        "connection": {
-            "friend": "John",
-            "landlord": "Zalika"
+        "relations": {
+            "Zalika": "friend",
+            "John": "partner"
         }
     }
-
+    ,"Zalika": {
+        "age": 28,
+        "job": "artist",
+        "relations": {
+            "Jill": "friend"
+        }
+    }
+    ,
+    "John": {
+        "age": 27,
+        "job": "writer",
+        "relations": {
+            "Jill": "partner"
+        }
+    },
+    "Nash": {
+        "age": 34,
+        "job": "chef",
+        "relations": {
+            "John": "cousin",
+            "Zalika": "landlord"
+        }
+    }
 }
-print(my_group)
+
+# the maximum age of people in the group
+age_max = max([int(person['age']) for person in my_group.values()])
+
+# the average (mean) number of relations among members of the group
+from numpy import average
+relations_mean = average([len(person['relations']) for person in my_group.values()])
+
+# the maximum age of people in the group that have at least one relation
+age_max_relations = max([person['age'] for person in my_group.values() if int(len(person['relations']) > 1)])
+
+# [more advanced] the maximum age of people in the group that have at least one friend
+age_max_friends = max([person['age'] for person in my_group.values() if 'friend' in person['relations'].values()])
+
+print(age_max_friends)
