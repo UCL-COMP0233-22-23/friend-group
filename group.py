780e24a2066c93a593298c91fe8d72c43094347c
Anda-Raluca Epure <anda.epure.20@ucl.ac.uk>
2022-10-20 10:56:17 +0100

    Create friends' group

--- group.py
+++ group.py
@@ -1,5 +1,24 @@
 """An example of how to represent a group of acquaintances in Python."""
 
-# Your code to go here...
+my_group = [{"name":"Jill", 
+        "job":"biologist",
+        "relationship":[("friend","Zalika"),("partner","John")],
+        "age":26},
+        {"name":"Zalika", 
+        "job":"artist",
+        "relationship":[("friend","Jill")],
+        "age":28},
+        {"name":"John", 
+        "job":"writer",
+        "relationship":[("partner","Jill")],
+        "age":27},
+         {"name":"Nash", 
+        "job":"chef",
+        "relationship":[("landlord","Zalika"),("cousin","John")],
+        "age":34}
+        ]
 
-my_group =
+#print(my_group[0]['name'] + " is " + str(my_group[0]["age"]) +", a " + my_group[0]["job"] +" and she is " + my_group[0]['relationship'][0][1] + "'s " + my_group[0]['relationship'][0][0])
+
+
+
