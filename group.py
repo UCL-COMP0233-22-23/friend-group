4129034f9d1afbeeae258965b21fd254f47d2d2f
YRY <54588936+YRYoung@users.noreply.github.com>
2022-10-20 10:47:44 +0100

    Update group.py

--- group.py
+++ group.py
@@ -1,5 +1,13 @@
 """An example of how to represent a group of acquaintances in Python."""
 
 # Your code to go here...
-
+class Person:
+  def __init__(self, name, age, connection={},job=None):
+    self.name = name
+    self.age = age
+    self.job=job
+    self.connection=connection
+    
+    
+    
 my_group =
