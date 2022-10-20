a6773fb366d08857c169560d57e80e2d1b57591f
YRY <54588936+YRYoung@users.noreply.github.com>
2022-10-20 10:55:05 +0100

    Update group.py
    Create a Class for each person
    Add examples

--- group.py
+++ group.py
@@ -1,5 +1,16 @@
 """An example of how to represent a group of acquaintances in Python."""
+class Person:
+  def __init__(self, name, age, connection={},job=None):
+    self.name = name
+    self.age = age
+    self.job=job
+    self.connection=connection
+    
+    
+    
+Jill=Person('Jill',26,{'friend':'Zalika','partner':'John'},'biologist')
+Zalika=Person('Zalika',28, {'friend':'Jill'},'artist')
 
-# Your code to go here...
 
-my_group =
+my_group =[Jill,Zalika]
+
