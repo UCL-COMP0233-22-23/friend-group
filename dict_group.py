4a1883f2f12c0ad00c88c57051eb5e4255413a3b
Q <ucapwqi@ucl.ac.uk>
2022-10-20 11:16:25 +0100

    Group in dictionary

---
+++ dict_group.py
@@ -0,0 +1,12 @@
+John = {
+    'age' : 21,
+    'job' :'student',
+    'connection' : ["Ann's friend","B's friend"] 
+    }
+
+Ann = {
+    'age' : 20,
+    'job' : 'student',
+    'connection' : ["John's friedn"]
+}
+
