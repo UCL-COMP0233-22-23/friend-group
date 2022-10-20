bb8b640932a1e3d3493cb44bf30089e28762f0d8
QiyuChen1997 <rmapqyc@ucl.ac.uk>
2022-10-20 10:51:03 +0100

    'stretch goal 09, from Qiyu'

--- group.py
+++ group.py
@@ -2,4 +2,28 @@
 
 # Your code to go here...
 
-my_group =
+class People:
+    def __init__(self, name, age, job=None):
+        self.name = name
+        self.age = age
+        self.job = job
+        self.relation = []
+
+    def connect(self, name, relationship):
+        self.relation.append({'name': name, 'relationship': relationship})
+    
+
+jill = People('Jill', 26, 'biologist')
+zalika = People('Zalika', 28, 'artist')
+john = People('John', 27, 'writer')
+nash = People('Nash', 34, 'chef')
+jill.connect(zalika.name, 'friend')
+jill.connect(john.name, 'partner')
+zalika.connect(jill.name, 'friend')
+john.connect(jill.name, 'partner')
+nash.connect(john.name, 'cousin')
+
+
+my_group = [jill, zalika, john, nash]
+
+print('here')
