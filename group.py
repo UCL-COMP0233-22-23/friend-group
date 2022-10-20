9061a1f2a69aabad931ba6101d318b1e878d736f
Torr <torr.fischman@gmail.com>
2022-10-20 10:48:35 +0100

    Written initial dict

--- group.py
+++ group.py
@@ -1,5 +1,12 @@
-"""An example of how to represent a group of acquaintances in Python."""
+#An example of how to represent a group of acquaintances in Python.
 
-# Your code to go here...
+friends_dict = {
+  "Name": ["Jill", "Zalika", "John", "Nash"],
+  "Age": [26, 28, 27, 34],
+  "Job": ["Biologist", "Artist", "Writer", "Chef"],
+  "Connection": ["Zalika's friend and John's partner", "Jill's friend,", "Jill's Partner", "John's cousin and Zalika's landlord"],
+}
 
-my_group =
+#my_group =
+
+print(friends_dict)
