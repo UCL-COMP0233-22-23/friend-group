d46541f21e0fafa762fbb8440c6b66d0aeaf8b4c
maruftalukdar123 <108669705+maruftalukdar123@users.noreply.github.com>
2022-10-20 11:03:04 +0100

    Update group by using classes
    Friend group using classes

--- group.py
+++ group.py
@@ -1,5 +1,13 @@
 """An example of how to represent a group of acquaintances in Python."""
 
-# Your code to go here...
+class friend_group():
+    def __init__(self,name,age,job,connection):
+        self.name = name
+        self.age = age
+        self.job = job
+        self.connection = connection
+        
+friend1 = friend_group('Wei',21,'Student',['Maruf friend','Random'])
+friend2 = friend_group('Maruf',21,'Student','Weis friend')
 
-my_group =
+friend1.name
