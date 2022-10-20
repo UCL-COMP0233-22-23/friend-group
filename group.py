a3ebde9e552cae359e725b559973ba5522f9f80a
Sheila <zcahsmp@ucl.ac.uk>
2022-10-20 11:13:56 +0100

    Sheila attempt

--- group.py
+++ group.py
@@ -2,4 +2,27 @@
 
 # Your code to go here...
 
-my_group =
+my_group = "Paul Nathan and Sheila Perez"
+
+
+class Friend:
+    def _init__(self,name,age,job,relations):
+        self.name = name
+        self.age = age
+        self.job = job
+        self.relations = {}
+
+    def add_relation(self,friend,relation):
+        self.relations[friend] = relation
+
+
+    
+
+jill = Friend('Jill','26','Biologist',{'Zalika':'friend','John':'partner'})
+zalika = Friend('Zalika','28','Artist',{'Jill':'friend'})
+john = Friend('John','27','Writer'{'Jill':'partner'})
+nash = Friend('Nash','34','Chef',{'John':'cousin','Zalika':'landlord'})
+
+
+
+
