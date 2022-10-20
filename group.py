4ea83e2077b973645d2c8ae002e2c9f58e4379a3
Shameer <shameer.ahmad.19@ucl.ac.uk>
2022-10-20 10:56:08 +0100

    made group dictionary

--- group.py
+++ group.py
@@ -2,4 +2,37 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {
+            'Raj': {
+                'age': 21,
+                'job': 'checkout_supervisor',
+                'connections': {
+                    'Surabhi': 'friend',
+                    'Shameer': 'friend'
+                    }
+            }
+            
+            , 
+            
+            'Surabhi': {
+                'age': 21,
+                'job': 'data_analyst',
+                'connections': {
+                    'Raj': 'friend',
+                    'Shameer': 'friend'
+                }
+            }, 
+            
+            'Shameer' : {
+                'age': 21,
+                'job': 'tutor',
+                'connections': {
+                    'Surabhi': 'friend',
+                    'Raj': 'friend'
+                    } 
+            }
+            
+            
+        
+    }
+        
