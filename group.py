05bd208310394bc811b7d83bb98dc9773010eca6
Chongfeng Ling <ling.chongfeng@outlook.com>
2022-10-20 10:39:23 +0100

    add a group of people by dict

--- group.py
+++ group.py
@@ -2,4 +2,34 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {
+    "Jill" : {
+        "age": 26,
+        "job": 'biologist',
+        'friend': 'Zalika',
+        "partner": "John"
+    },
+    "Zalika" : {
+    "name": "Zalika",
+    "age": "28",
+    "job": "artist",
+    "friend": "Jill",
+    "partner": ""
+    },
+    "John" : {
+    "name": "John",
+    "age": "27",
+    "job": "writer",
+    "friend": "",
+    "partner": "Jill"
+    },
+    "Nash" : {
+    "name": "Nash",
+    "age": "34",
+    "job": "chef",
+    "cousin": "John",
+    "landlord": "Zalika"
+    }
+
+}
+print(my_group)
