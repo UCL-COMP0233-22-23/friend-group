ecd012cb09a4355e4c41a8cc3b270634ded36c61
yumengliuwendy <114681620+yumengliuwendy@users.noreply.github.com>
2022-10-20 10:53:11 +0100

    basic information about people

--- group.py
+++ group.py
@@ -1,5 +1,4 @@
-"""An example of how to represent a group of acquaintances in Python."""
-
-# Your code to go here...
-
-my_group =
+lym={'name':'lym','age':22,'job':'student','relationship':'friend with zcx'}
+zcx={'name':'zcx','age':22,'job':'student','relationship':'friend with lym'}
+jill={'name':'jill','age':26,'job':'biologist','relationship':'partner with john'}
+john={'name':'john','age':30,'job':'CEO','relationship':'partner with jill'}
