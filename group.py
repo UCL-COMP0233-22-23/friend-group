8e16b009a4f0de6863b7aea7c81d437a71bf1878
Shijie Cao <ucapaox@ucl.ac.uk>
2022-10-20 11:47:26 +0100

    Shijie-Joanne first commit

--- group.py
+++ group.py
@@ -1,5 +1,50 @@
 """An example of how to represent a group of acquaintances in Python."""
 
 # Your code to go here...
+group = {
+    "Jill" : {"name":"Jill", 
+            "age":26, "job":"a biologist", 
+            "connction":{
+            "friend": "Zalika",
+            "partner":"John"}
+    },
+    "Zalika" : {"name":"Zalika", 
+            "age":28, "job":"an artist", 
+            "connection":{
+                "friend": "Jill",
+                "rent":"Nash"}
+    },
+    "John" : {"name":"John",
+            "age":27,
+            "job":"a writer",
+            "connection":{
+                "partner":"Jill",
+                "cousin":"Nash"}
+    },
+    "Nash" : {"name":"Nash",
+            "age":34, "job":"a chef", 
+            "connection":{
+                "cousin":"John", 
+                "landlord":"Zalika"}
+    }
+}
 
-my_group =
+#print(group)
+
+#for i in range(len(group)):
+#    print(group[i]['name'],"is",group[i]['age'],",",group[i]['job'], 'and')
+
+print(group.keys())
+
+
+
+
+
+
+
+
+
+
+
+
+
