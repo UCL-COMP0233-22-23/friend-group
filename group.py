4df1004b47af4ea9637b37f2d6e298487e527f30
YRY <54588936+YRYoung@users.noreply.github.com>
2022-10-20 11:11:59 +0100

    Update group.py

--- group.py
+++ group.py
@@ -15,3 +15,16 @@ John=Person('John',27,{'partner':'Jill'},'writer')
 
 my_group =[Jill,Zalika,John]
 
+
+def forget(person1, person2):
+  pass
+
+# which removes the connection between two people in the group
+def add_person(name, age, job, relations):
+  pass
+
+# which adds a new person with the given characteristics to the group
+def average_age():
+  pass
+# which calculates the mean age for the group
+
