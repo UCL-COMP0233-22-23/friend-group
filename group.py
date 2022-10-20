7a846167c3ad568cb06d6a5d7f8c25c91397cd11
YOU ZUO <zuoyou1998@gmail.com>
2022-10-20 10:57:17 +0100

    add group structure

--- group.py
+++ group.py
@@ -2,4 +2,37 @@
 
 # Your code to go here...
 
-my_group =
+my_group ={
+    'Jill':{
+        'age': 26,
+        'job':'biologist',
+        'conection':{
+            'Zalika':'friend',
+            'John': 'partner'
+        }
+    },
+    'Zalika':{
+        'age': 28,
+        'job':'artist',
+        'conection':{
+            'Jill':'friend',
+        }
+    },
+    'John':{
+        'age': 27,
+        'job':'writer',
+        'conection':{
+            'Jill':'partner',
+        }
+    },
+    'Nash':{
+        'age': 34,
+        'job':'chef',
+        'conection':{
+            'John': 'cousin',
+            'Zalika':'landlord'
+        }
+    },
+}
+
+
