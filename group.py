41b78ea50c1f0e027527bbb3d6c8bb9e764bddc7
Chongfeng Ling <ling.chongfeng@outlook.com>
2022-10-20 10:55:48 +0100

    add nested connection dict

--- group.py
+++ group.py
@@ -5,30 +5,38 @@
 my_group = {
     "Jill" : {
         "age": 26,
-        "job": 'biologist',
-        'friend': 'Zalika',
-        "partner": "John"
+        "job": "biologist",
+        "connection": {
+            "friend": "Zalika",
+            "partner": "John"
+        }
     },
     "Zalika" : {
     "name": "Zalika",
     "age": "28",
     "job": "artist",
-    "friend": "Jill",
-    "partner": ""
+    "connection": {
+        "friend": "Jill",
+        "partner": ""
+        }
     },
     "John" : {
     "name": "John",
     "age": "27",
     "job": "writer",
-    "friend": "",
-    "partner": "Jill"
+    "connection": {
+        "friend": "",
+        "partner": "Jill"
+        }
     },
     "Nash" : {
     "name": "Nash",
     "age": "34",
     "job": "chef",
-    "cousin": "John",
-    "landlord": "Zalika"
+    "connection": {
+        "friend": "John",
+        "landlord": "Zalika"
+        }
     }
 
 }
