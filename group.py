f717f98779269e29476da5329984b1e34ee9e8ef
iynix <ucaphud@ucl.ac.uk>
2022-10-20 11:39:54 +0100

    use class to sort

--- group.py
+++ group.py
@@ -2,19 +2,17 @@
 
 # Your code to go here...
 
-my_group =
+#my_group =
 
-'''class Person:
-    def _init_(self,Name,age,friend,partner,cousin,tenant):
-    self.name = name
-    self.age = age
-    self.friend = friend
-    self.partner = partner
-    self.cousin = cousin
-    self.tenant = tenant
+class Person:
+    def __init__(self,name,age,job,relationship):
+        self.name = name
+        self.age = age
+        self.job = job
+        self.relationship = relationship
 
-Jill = Person('Jill','26','biologist','Zalika','John','','')
-print(Jill.name)'''
-
-my_group = {['Name':'Jill', 'age':'26','job':'biologist','friend':'Zalika','partner':'John'],['Name':'Zalika','age':'28','job':'artist','friend':'Jill'],['Name':'John','job':'writer','partner':'Jill'],['Name':'Nash','job':'chef','cousin':'John','tenant':'Zalika']}
+Jill = Person('Jill','26','biologist',[['friend','Zalika'],['partner','John']])
+print(Jill.name)
+print(Jill.relationship)
+#my_group = {['Name':'Jill', 'age':'26','job':'biologist','friend':'Zalika','partner':'John'],['Name':'Zalika','age':'28','job':'artist','friend':'Jill'],['Name':'John','job':'writer','partner':'Jill'],['Name':'Nash','job':'chef','cousin':'John','tenant':'Zalika']}
 #print(my_group)
