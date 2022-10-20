7cae8a7ea6bc8d424fa030c1f786b21a58888fe5
Zhenghao Lu <ucapzlu@ucl.ac.uk>
2022-10-20 11:11:42 +0100

    from zhenghao

--- group.py
+++ group.py
@@ -1,5 +1,16 @@
 """An example of how to represent a group of acquaintances in Python."""
 
-# Your code to go here...
+my_dictionary = {'key1': 1, 'key4': 'abc'}
+my_dictionary['key1']
+my_dictionary['key4']
 
-my_group =
+my_group = {
+    'Zhenghao': {
+        'name': 'Zhenghao', 
+        'age': 22,
+        'job': 'a student',
+        'relations': 'lanqiming friend',
+    }
+}
+
+print(my_group['Zhenghao']['name'], 'is', my_group['Zhenghao']['age'])
