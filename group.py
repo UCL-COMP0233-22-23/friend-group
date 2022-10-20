11e3590b84209cf21797b6d220eabc91a4b68080
William WEI <ucaphwe@ucl.ac.uk>
2022-10-20 10:43:00 +0100

    add partners to my list

--- group.py
+++ group.py
@@ -2,4 +2,23 @@
 
 # Your code to go here...
 
-my_group =
+my_group =[
+    
+    {
+        'name' : 'William',
+        'age' : 22,
+        'job' : 'programmer',
+        'connections' : {
+            'son' : 'Jack'
+        }
+    },
+    {
+        'name' : 'Jack',
+        'age' : 20,
+        'job' : 'boss',
+        'connections' : {
+            'dad' : 'William',
+            
+        }
+    }
+]
