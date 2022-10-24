444fa214ec58e3fa9221e9d0a0639dd56add6278
ruihuang888 <rui.huang.20@outlook.com>
2022-10-24 21:31:19 +0100

    add-group-data-model

--- group.py
+++ group.py
@@ -2,4 +2,41 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {
+    "Jill": {
+        "age": 26,
+        "job": "biologist",
+        "relations": {
+            "friend":"Zalika",
+            "partner":"John"
+        }
+    },
+    "Zalika": {
+        "age": 28,
+        "job": "artist",
+        "relations": {
+            "friend":"Jill",
+            "partner":""
+        }
+    },
+    "John": {
+        "age": 27,
+        "job": "writer",
+        "relations": {
+            "friend":"",
+            "partner":"Jill"
+        }
+    },
+    "Nash": {
+        "age": 34,
+        "job": "chef",
+        "relations": {
+            "friend":"",
+            "partner":"",
+            "cousin":"John",
+            "landlord":"Zalika" 
+        }
+}
+}
+
+print(my_group)
