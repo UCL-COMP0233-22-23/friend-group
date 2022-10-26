ffb5590f1d828bf64b68f7853698d0b02f8d47aa
Q <ucapwqi@ucl.ac.uk>
2022-10-26 22:43:49 +0100

    Summary data from group_wei

--- dict_group.py
+++ dict_group.py
@@ -1,3 +1,6 @@
+from statistics import mean
+
+
 group = {
     
     "John" : {
@@ -21,6 +24,23 @@ group = {
     "Zalika": {
     "age": 28,
     "job": "artist",
-    "relations": {"Ann": "cousin"}
+    "connection": {"Ann": "cousin"}
     },
 }
+
+# the maximum age of people in the group
+ages = [i["age"] for i in group.values()]
+print("The maximum age of people in the group is", max(ages))
+
+# the average (mean) number of relations among members of the group
+num_relation = [len(i["connection"]) for i in group.values()]
+mean_relation = mean(num_relation)
+print("The average (mean) number of relations among members of the group is", mean_relation)
+
+# the maximum age of people in the group that have at least one relation
+age_rel = [i["age"] for i in group.values() if len(i["connection"]) >= 1]
+print("the maximum age of people in the group that have at least one relation is",max(age_rel))
+
+#  the maximum age of people in the group that have at least one friend
+age_friend = [i["age"] for i in group.values() if "friend" in i["connection"].values()]
+print("the maximum age of people in the group that have at least one friend is", max(age_friend))
