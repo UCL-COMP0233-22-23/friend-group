b92c86818ce9a462348e7c2ecd4c34b5726c1200
wxyi <weixi.yi.22@ucl.ac.uk>
2022-10-26 19:36:36 +0100

    homework01

--- group.py
+++ group.py
@@ -1,6 +1,14 @@
-"""An example of how to represent a group of acquaintances in Python."""
+import json
 
-# Your code to go here...
+mydata = {'key': ['value1', 'value2'], 
+          'key2': {'key4':'value3'}}
 
-my_group = {'namae':'Jill','job':'biologist','age':'26','connection':"Zalika's friend and John's partner"}
+json.dumps(mydata)
+with open('mylife.json', 'r') as json_file:
+    my_data_as_string = json_file.read()
+mydata = json.loads(my_data_as_string)
+mydata['somekey']
+print(json.dumps(mydata, indent=4))
+
+
 
