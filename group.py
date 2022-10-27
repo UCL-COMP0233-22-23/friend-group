5a0fcec6863dbaddb55019aa954b62259e0c6b8a
Shijie Cao <ucapaox@ucl.ac.uk>
2022-10-27 01:20:35 +0100

    summary data from group

--- group.py
+++ group.py
@@ -1,50 +1,62 @@
-"""An example of how to represent a group of acquaintances in Python."""
-
-# Your code to go here...
 group = {
-    "Jill" : {"name":"Jill", 
-            "age":26, "job":"a biologist", 
-            "connction":{
-            "friend": "Zalika",
-            "partner":"John"}
+    "Jill": {
+        "age": 26,
+        "job": "biologist",
+        "relations": {
+            "Zalika": "friend",
+            "John": "partner"
+        }
     },
-    "Zalika" : {"name":"Zalika", 
-            "age":28, "job":"an artist", 
-            "connection":{
-                "friend": "Jill",
-                "rent":"Nash"}
+    "Zalika": {
+        "age": 28,
+        "job": "artist",
+        "relations": {
+            "Jill": "friend"
+        }
     },
-    "John" : {"name":"John",
-            "age":27,
-            "job":"a writer",
-            "connection":{
-                "partner":"Jill",
-                "cousin":"Nash"}
+    "John": {
+        "age": 27,
+        "job": "writer",
+        "relations": {
+            "Jill": "partner"
+        }
     },
-    "Nash" : {"name":"Nash",
-            "age":34, "job":"a chef", 
-            "connection":{
-                "cousin":"John", 
-                "landlord":"Zalika"}
+    "Nash": {
+        "age": 34,
+        "job": "chef",
+        "relations": {
+            "John": "cousin",
+            "Zalika": "landlord"
+        }
     }
 }
+#the maximum age of people in the group
+ages = []
+for i in group:
+    age = group[i]['age']
+    ages.append(age)
+print("the maximum age of people in the group:", max(ages))
 
-#print(group)
+#the average (mean) number of relations among members of the group
+rel_nums = 0
+for i in group:
+    rel_num = len(group[i]['relations'])
+    rel_nums +=rel_num
+ave_num = rel_nums / len(group)
+print("the average (mean) number of relations among members of the group:", ave_num)
 
-#for i in range(len(group)):
-#    print(group[i]['name'],"is",group[i]['age'],",",group[i]['job'], 'and')
+#the maximum age of people in the group that have at least one relation
+ages = []
+for i in group:
+    if len(group[i]['relations']) >= 1:
+        age = group[i]['age']
+        ages.append(age)
+print("the maximum age of people in the group that have at least one relation", max(ages))
 
-print(group.keys())
-
-
-
-
-
-
-
-
-
-
-
-
-
+#the maximum age of people in the group that have at least one friend
+ages = []
+for i in group:
+    if 'friend' in group[i]['relations'].values():
+        age = group[i]['age']
+        ages.append(age)
+print("the maximum age of people in the group that have at least one friend", max(ages))
