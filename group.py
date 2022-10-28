8fdf02ac9018ef35dffec17b7db00bcfea0be379
Rongru <ucjurc6@ucl.ac.uk>
2022-10-28 18:39:42 +0100

    added forget, new people and average age function

--- group.py
+++ group.py
@@ -20,3 +20,26 @@ my_group = {"Jill": {"Age": 26,
             "Job": "chef",
             "Relationship": {"John": "cousin", 
             "Zalika": "landlord"}}}
+
+
+def forget(person1, person2):
+    for name, info in my_group.items():
+        RelationDic = info["Relationship"]
+        if name == person1:
+            RelationDic.pop(person2, None)
+        if name == person2:
+            RelationDic.pop(person1, None)
+
+def add_person(name, age, job, relations = {}):
+    my_group[name] = {"Age": age, "Job": job, "Relationship": relations }
+    
+def average_age():
+    age = 0
+    count_num = 0
+    for name, info in my_group.items():
+        age += info["Age"]
+        count_num += 1
+    average = age/count_num
+    print(f"The average age of {count_num} people in the group is {average}")
+
+ 
