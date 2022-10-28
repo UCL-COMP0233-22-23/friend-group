afed44b01b983b799987e3587dca3cf84443744e
Rongru <ucjurc6@ucl.ac.uk>
2022-10-28 17:01:28 +0100

    first_version by RR

--- group.py
+++ group.py
@@ -1,5 +1,22 @@
 """An example of how to represent a group of acquaintances in Python."""
 
+
 # Your code to go here...
 
-my_group =
+my_group = {"Jill": {"Age": 26, 
+            "Job": "biologist", 
+            "Relationship": {"Zalika": "friend",
+            "partner": "John"}},
+            
+            "Zalika": {"Age": 28, 
+            "Job": "artist", 
+            "Relationship": {"Jill": "friend"}},
+
+            "John": {"Age": 28,
+            "Job": "writer",
+            "Relationship": {"Jill": "partner"}},
+
+            "Nash": {"Age": 34,
+            "Job": "chef",
+            "Relationship": {"John": "cousin", 
+            "Zalika": "landlord"}}}
