94db88116d416b784796b422d2bcf2d333ced3e9
Weixuan Xing <rmapxin@ucl.ac.uk>
2022-10-30 16:50:53 +0000

    groupset

--- group.py
+++ group.py
@@ -2,4 +2,37 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {
+    "Jack":{
+        "age":25,
+        "job": "history teacher",
+        "relations":{
+            "Sam": "friend",
+            "Alex":"partner",
+            "Zoe": "brother"
+        }
+    },
+    "Sam":{
+        "age":27,
+         "job": "bartender",
+        "relations":{
+            "Jack": "friend",
+            "Alex": "friend"
+        }
+    },
+    "Zeo":{
+        "age":19,
+         "job": "student",
+        "relations":{
+            "Jack": "sister",
+        }
+    },
+    "Alex":{
+        "age":22,
+         "job": "archeologist",
+        "relations":{
+            "Jack": "partner",
+            "Alex": "friend"
+        }
+    }
+}
