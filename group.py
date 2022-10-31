f095f8ad6346ccddf57a56262b87629bdeb43f1c
Rongru <ucjurc6@ucl.ac.uk>
2022-10-31 11:48:30 +0000

    added summary data in comprehensive expression

--- group.py
+++ group.py
@@ -2,11 +2,10 @@
 
 
 # Your code to go here...
-
 my_group = {"Jill": {"Age": 26, 
             "Job": "biologist", 
             "Relationship": {"Zalika": "friend",
-            "partner": "John"}},
+            "John": "partner"}},
             
             "Zalika": {"Age": 28, 
             "Job": "artist", 
@@ -21,6 +20,9 @@ my_group = {"Jill": {"Age": 26,
             "Relationship": {"John": "cousin", 
             "Zalika": "landlord"}}}
 
+#forget(person1, person2) which removes the connection between two people in the group
+#add_person(name, age, job, relations) which adds a new person with the given characteristics to the group
+#average_age() which calculates the mean age for the group
 
 def forget(person1, person2):
     for name, info in my_group.items():
@@ -42,4 +44,21 @@ def average_age():
     average = age/count_num
     print(f"The average age of {count_num} people in the group is {average}")
 
- 
+###Try some of the functions
+#add_person("Rongru",24,"Student",{"John": "friend"})
+
+#forget("Jill","Zalika")
+#my_group
+
+
+##Add the following comprehensive relations
+#the maximum age of people in the group
+#the average (mean) number of relations among members of the group
+#the maximum age of people in the group that have at least one relation
+#[more advanced] the maximum age of people in the group that have at least one friend
+
+MaxiAge_comprehend = max([info["Age"] for name, info in my_group.items()])
+Mean_relation = sum([len(info["Relationship"]) for name, info in my_group.items()])/len(my_group.keys())
+MaxAge_relation = max([info["Age"] for name, info in my_group.items() if len(info["Relationship"]) >1])
+MaxAge_friend = max([info["Age"] for name, info in my_group.items() if "friend" in info["Relationship"].values()])
+
